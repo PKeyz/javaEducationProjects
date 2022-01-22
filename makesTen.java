29834e2535635b6932a77411e26da4e05c5760fc
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-01-22 17:25:51 +0100

    Create makesTen.java
    I feel something is wrong, but I don't get it yet, work with booleans have to look different

---
+++ makesTen.java
@@ -0,0 +1,14 @@
+public boolean makes10(int a, int b) {
+  boolean isTen;
+  
+  if (a+b == 10 || a == 10 || b == 10)
+  {
+    isTen = true;
+    return isTen;
+  }
+  else
+  {
+    isTen = false;
+    return isTen;
+  }
+}
