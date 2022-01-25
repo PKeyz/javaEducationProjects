24383f07ebca0b5fa76c5dfccdc86a981f95c18e
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-01-25 23:12:46 +0100

    Create posNeg.java

---
+++ posNeg.java
@@ -0,0 +1,23 @@
+
+    public boolean posNeg(int a, int b, boolean negative) {
+        boolean different;
+        
+        if(a > 0 && b < 0 || a < 0 && b > 0)
+        {
+          different = true;
+          return different;
+        }
+        else if(negative == true && (a+b)<0 )
+        {
+          different = true;
+          return different;
+        }
+        else()
+        {
+          different = false;
+          return different;
+        }
+        
+      }    
+
+
