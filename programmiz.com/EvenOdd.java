e05b76ccd6443307099ca4c9ef650c9c777a8a57
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-02-09 19:39:37 +0100

    Update EvenOdd.java
    the ternary operator is definitely not a habit yet even though its useful

--- programmiz.com/EvenOdd.java
+++ programmiz.com/EvenOdd.java
@@ -4,6 +4,7 @@ public class EvenOdd
 {
     public static void main(String[] args) 
     {
+       /*
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
 
@@ -13,5 +14,16 @@ public class EvenOdd
         }
         else
             System.out.println(num + "is odd"); 
-    }
+            */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        int num = sc.nextInt();
+
+        String evenOdd = (num % 2 == 0) ? "even" : "odd";
+
+        System.out.println(num + " is " + evenOdd);
+    } 
 }
