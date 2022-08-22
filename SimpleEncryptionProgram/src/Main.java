e03fe4fb312c9919f4288d2cfff72be8314fbdb7
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-08-22 13:31:27 +0200

    next project: small OTP encryption program;

---
+++ SimpleEncryptionProgram/src/Main.java
@@ -0,0 +1,22 @@
+package src;
+
+/**
+ * source: https://www.youtube.com/watch?v=jjcJCvIoj2M&list=PLZPZq0r_RZOMhCAyywfnYLlrjiVOkdAI1&index=90
+ *
+ * OTP = One-time pad
+ *
+ * Encryption technique that cannot be cracked,
+ * but requires the use of a one-time pre-shared
+ * key the same size as,or longer than, the message
+ * being sent.
+ * In this technique, plaintext is paired with random secret key (also preferred to as a one-time pad
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        EncryptionProgram encryption = new EncryptionProgram();
+
+    }
+}
