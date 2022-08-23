02c37051c4dcbaac0305626c1fd116ca25274d6a
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-08-23 23:53:19 +0200

    new small project - from youtube to code along : TextEditor;

---
+++ JavaTextEditorApp/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * ressource: https://www.youtube.com/watch?v=NKjqAQAtq-g&list=PLZPZq0r_RZOMhCAyywfnYLlrjiVOkdAI1&index=90
+ * Java Text Editor App - simple:
+ *
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
