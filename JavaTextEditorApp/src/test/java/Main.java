8f688f6defaedf55b94109399bb3e23e156808d4
Phil Keyz <53272112+Morrgoth@users.noreply.github.com>
2022-08-24 20:10:32 +0200

    added basic editor functionality, like font size; color ; font

--- JavaTextEditorApp/src/test/java/Main.java
+++ JavaTextEditorApp/src/test/java/Main.java
@@ -6,7 +6,9 @@
 
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TextEditor textEditor = new TextEditor();
     }
+
 }
---
+++ JavaTextEditorApp/src/test/java/TextEditor.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextEditor extends JFrame implements ActionListener {
+
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JLabel fontLabel;
+    JSpinner fontSizeSpinner;
+    JButton fontColorButton;
+    JComboBox fontBox;
+
+    TextEditor(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Keyz Editor");
+        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+
+        this.textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("Arial",Font.PLAIN,20));
+
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(450,450));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        fontLabel = new JLabel("Font Size");
+
+        fontSizeSpinner = new JSpinner();
+        fontSizeSpinner.setPreferredSize(new Dimension(50,25));
+        fontSizeSpinner.setValue(20);
+        fontSizeSpinner.addChangeListener(new ChangeListener() {
+
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                textArea.setFont(new Font(textArea.getFont().getFamily(),Font.PLAIN,(int)fontSizeSpinner.getValue()));
+            }
+        });
+        fontColorButton = new JButton("Color");
+        fontColorButton.addActionListener(this);
+
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        fontBox = new JComboBox(fonts);
+        fontBox.addActionListener(this);
+        fontBox.setSelectedItem("Arial");
+
+        this.add(fontLabel);
+        this.add(fontSizeSpinner);
+        this.add(fontColorButton);
+        this.add(fontBox);
+        this.add(scrollPane);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == fontColorButton){
+            JColorChooser colorChooser = new JColorChooser();
+
+            Color color = colorChooser.showDialog(null,"Choose a color", Color.BLACK);
+
+            textArea.setForeground(color);
+        }
+        if(e.getSource() == fontBox){
+            textArea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN,textArea.getFont().getSize()));
+
+        }
+    }
+}
